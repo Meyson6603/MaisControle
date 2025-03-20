e861a6c20a8794cd659f572fa762c1a29c4adda4
wanessaneves <wanessanevsm@gmail.com>
2025-03-20 11:22:05 -0300

    refactor: update server configuration, remove unused console log and install dotenv

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
-require('dotenv').config({ path: './config/.env' })
-const express = require('express')
-const app = express()
-const path = require('path')
-app.use(express.json())
-app.use(express.static('./public'))
-app.get('*', (req, res) => {
-    const filePath = path.join(__dirname, 'public', 'index.html')
-    res.sendFile(filePath)
-})
-app.listen(process.env.PORT, () => console.log(`Server is running on port ${process.env.PORT}`))
+require("dotenv").config({ path: "./src/config/.env" });
+const express = require("express");
+const app = express();
+const path = require("path");
+app.use(express.json());
+app.use(express.static("./public"));
+app.get("*", (req, res) => {
+  const filePath = path.join(__dirname, "public", "index.html");
+  res.sendFile(filePath);
+});
+app.listen(process.env.PORT, () =>
+  console.log(`Server is running on port ${process.env.PORT}`)
+);
