155741307d4034b396beb8c4f4b93a68569876b9
Leandro Lávila Dias Souza <leandrolavila16@gmail.com>
2025-03-26 01:49:44 +0000

    feat: implement user controller with CRUD operations

---
+++ src/controllers/userController.js
@@ -0,0 +1,59 @@
+const userService = require('../services/userService');
+
+const createUser = async (req, res, next) => {
+    try {
+        const user = req.body;
+        const newUser = await userService.createUser(user);
+        res.status(201).json(newUser);
+    } catch (error) {
+        next(error);
+    }
+}
+
+const listUsers = async (req, res, next) => {
+    try {
+        const users = await userService.listUsers();
+        res.status(200).json(users);
+    } catch (error) {
+        next(error);
+    }
+}
+
+const getUser = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const user = await userService.getUser(id);
+        res.status(200).json(user);
+    } catch (error) {
+        next(error);
+    }
+}
+
+const updateUser = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const user = req.body;
+        const updatedUser = await userService.updateUser(id, user);
+        res.status(200).json(updatedUser);
+    } catch (error) {
+        next(error);
+    }
+}
+
+const deleteUser = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        await userService.deleteUser(id);
+        res.status(204).end();
+    } catch (error) {
+        next(error);
+    }
+}
+
+module.exports = {
+    createUser,
+    listUsers,
+    getUser,
+    updateUser,
+    deleteUser,
+};
