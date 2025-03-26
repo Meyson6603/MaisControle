322bf87f16ef9f57675e792378d55bb297a16cd8
Leandro Lávila Dias Souza <leandrolavila16@gmail.com>
2025-03-26 01:56:49 +0000

    style: fix indentation in errorHandler middleware

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,10 +1,9 @@
 const AppError = require('../utils/AppError');
 
 const errorHandler = (err, req, res, next) => {
-  const statusCode = err instanceof AppError ? err.statusCode : 500;
-  const message = err instanceof AppError ? err.message : 'Internal Server Error';
-  console.error (err);
-  res.status(statusCode).json({ erro: message });
+    const statusCode = err instanceof AppError ? err.statusCode : 500;
+    const message = err instanceof AppError ? err.message : 'Internal Server Error';
+    res.status(statusCode).json({ erro: message });
 };
 
 module.exports = errorHandler;
