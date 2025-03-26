593c8346fb22e53f1025877370acc40974d13fdf
Leandro Lávila Dias Souza <leandrolavila16@gmail.com>
2025-03-26 01:43:55 +0000

    feat: update router to include user routes and error handling middleware

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,6 +1,12 @@
-const router = require("express").Router();
+const express = require("express");
+const router = express.Router();
 const transactionRoutes = require("./transactionRoutes.js");
+const userRoutes = require("./userRoutes.js");
+const errorHandler = require("../middleware/errorHandler.js");
 
-router.use("/transactions", transactionRoutes);
+// router.use("/transactions", transactionRoutes);
+router.use("/users", userRoutes);
+
+router.use(errorHandler);
 
 module.exports = router;
