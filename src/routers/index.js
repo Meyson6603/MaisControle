5c2b10a27ae42989962a125a67837dde5cd6ba49
Leandro Lávila Dias Souza <leandrolavila16@gmail.com>
2025-03-26 01:49:22 +0000

    refactor: remove error handler middleware and comment transaction routes from router

--- src/routers/index.js
+++ src/routers/index.js
@@ -2,11 +2,8 @@ const express = require("express");
 const router = express.Router();
 const transactionRoutes = require("./transactionRoutes.js");
 const userRoutes = require("./userRoutes.js");
-const errorHandler = require("../middleware/errorHandler.js");
 
 // router.use("/transactions", transactionRoutes);
 router.use("/users", userRoutes);
-
-router.use(errorHandler);
 
 module.exports = router;
