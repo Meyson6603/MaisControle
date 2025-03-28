2edf95b2a413a32baa134bdc6ced1dda2f42eb63
Leandro Lávila Dias Souza <leandrolavila16@gmail.com>
2025-03-28 13:21:48 +0000

    refactor: update server.js to use env for port configuration

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const { data } = require("./src/config");
+const { env } = require("./src/config");
 const express = require("express");
 const path = require("path");
 const routes = require("./src/routers");
@@ -12,6 +12,6 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/api", routes);
 
 app.use(errorHandler);
-app.listen(data.PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${data.PORT}`);
+app.listen(env.PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${env.PORT}`);
 });
