163f8f96d732826b50d0aa3f5690ed1f50a44b67
wanessaneves <wanessanevsm@gmail.com>
2025-04-01 16:46:36 -0300

    fix: fixes minor errors and adjusts logic in stateManager.js

--- public/js/components/stateManager.js
+++ public/js/components/stateManager.js
@@ -1,20 +1,13 @@
-// Função para carregar as transações do localStorage
 const loadTransactions = () => {
-  const storedTransactions = localStorage.getItem("transactions");
-  if (storedTransactions) {
-    return JSON.parse(storedTransactions);
-  } else {
-    return {
-      expenses: [],
-      incomes: [],
-    };
-  }
+  const transactions = JSON.parse(localStorage.getItem("transactions")) || {
+    incomes: [],
+    expenses: [],
+  };
+  return transactions;
 };
 
-// Inicializando as transações a partir do localStorage
 let transactions = loadTransactions();
 
-// Função para salvar as transações no localStorage
 const saveTransactions = () => {
   localStorage.setItem("transactions", JSON.stringify(transactions));
 };
@@ -31,6 +24,10 @@ export const addTransaction = (type, transaction) => {
 
   console.log("Transações após adicionar:", transactions);
   updateSaldoGeral();
+  updateFaturasDoMes();
+  updateContasAReceber();
+  updateContasAPagar();
+  updateTotalUI();
 };
 
 // Função para obter o mês atual
@@ -63,21 +60,7 @@ const calculateMonthlyInvoices = () => {
     0
   );
 
-  const monthlyIncomes = transactions.incomes.filter((income) =>
-    isCurrentMonth(income.date)
-  );
-  const totalMonthlyIncomes = monthlyIncomes.reduce(
-    (total, income) => total + parseFloat(income.value || 0),
-    0
-  );
-
-  console.log("Despesas do Mês Atual:", totalMonthlyExpenses);
-  console.log("Receitas do Mês Atual:", totalMonthlyIncomes);
-
-  return {
-    totalMonthlyExpenses,
-    totalMonthlyIncomes,
-  };
+  return totalMonthlyExpenses;
 };
 
 // Para contas a pagar (despesas vencidas ou vencendo hoje)
@@ -89,8 +72,6 @@ const calculatePayableAccounts = () => {
     (total, expense) => total + parseFloat(expense.value || 0),
     0
   );
-
-  console.log("Contas a Pagar:", totalPayableExpenses);
 
   return totalPayableExpenses;
 };
@@ -105,63 +86,73 @@ const calculateReceivableAccounts = () => {
     0
   );
 
-  console.log("Contas a Receber:", totalReceivableIncomes);
-
   return totalReceivableIncomes;
 };
 
 // Função para calcular o saldo geral
 export const calculateSaldoGeral = () => {
-  const { totalMonthlyExpenses, totalMonthlyIncomes } =
-    calculateMonthlyInvoices();
+  const totalMonthlyIncomes = calculateTotalIncomesThisMonth();
+  const totalMonthlyExpenses = calculateMonthlyInvoices();
 
   const saldoGeral = totalMonthlyIncomes - totalMonthlyExpenses;
 
-  console.log("Saldo Geral Calculado:", saldoGeral);
-
   return saldoGeral;
+};
+
+const calculateTotalIncomesThisMonth = () => {
+  const monthlyIncomes = transactions.incomes.filter((income) =>
+    isCurrentMonth(income.date)
+  );
+  const totalMonthlyIncomes = monthlyIncomes.reduce(
+    (total, income) => total + parseFloat(income.value || 0),
+    0
+  );
+  return totalMonthlyIncomes;
+};
+
+const calculateTotalExpenses = () => {
+  return transactions.expenses.reduce(
+    (sum, expense) => sum + parseFloat(expense.value || 0),
+    0
+  );
+};
+
+const calculateTotalIncomes = () => {
+  return transactions.incomes.reduce(
+    (sum, income) => sum + parseFloat(income.value || 0),
+    0
+  );
 };
 
 // Função para atualizar o saldo geral
 export const updateSaldoGeral = () => {
   const saldoGeral = calculateSaldoGeral();
-
-  console.log("Saldo Geral Atualizado:", saldoGeral);
-
   const saldoElement = document.querySelector(
     ".cards-container .card:nth-child(1) .card__value"
   );
+
   if (saldoElement) {
     saldoElement.textContent = saldoGeral.toFixed(2);
-    if (saldoGeral < 0) {
-      saldoElement.classList.add("negative-value");
-      saldoElement.classList.remove("positive-value");
-    } else {
-      saldoElement.classList.add("positive-value");
-      saldoElement.classList.remove("negative-value");
-    }
+    saldoElement.classList.toggle("negative-value", saldoGeral < 0);
+    saldoElement.classList.toggle("positive-value", saldoGeral >= 0);
   } else {
     console.error("Elemento para saldo geral não encontrado.");
   }
 };
 
 export const updateFaturasDoMes = () => {
-  const { totalMonthlyExpenses } = calculateMonthlyInvoices();
-
-  console.log("Faturas do Mês Atualizadas:", totalMonthlyExpenses);
-
+  const totalMonthlyExpenses = calculateMonthlyInvoices();
   const faturasElement = document.querySelector(
     ".cards-container .card:nth-child(2) .card__value"
   );
+
   if (faturasElement) {
     faturasElement.textContent = totalMonthlyExpenses.toFixed(2);
-    if (totalMonthlyExpenses < 0) {
-      faturasElement.classList.add("negative-value");
-      faturasElement.classList.remove("positive-value");
-    } else {
-      faturasElement.classList.add("positive-value");
-      faturasElement.classList.remove("negative-value");
-    }
+    faturasElement.classList.toggle("negative-value", totalMonthlyExpenses < 0);
+    faturasElement.classList.toggle(
+      "positive-value",
+      totalMonthlyExpenses >= 0
+    );
   } else {
     console.error("Elemento para faturas do mês não encontrado.");
   }
@@ -170,21 +161,14 @@ export const updateFaturasDoMes = () => {
 // Função para atualizar contas a receber
 export const updateContasAReceber = () => {
   const totalReceivable = calculateReceivableAccounts();
-
-  console.log("Contas a Receber Atualizadas:", totalReceivable);
-
   const receberElement = document.querySelector(
     ".cards-container .card:nth-child(3) .card__value"
   );
+
   if (receberElement) {
     receberElement.textContent = totalReceivable.toFixed(2);
-    if (totalReceivable < 0) {
-      receberElement.classList.add("negative-value");
-      receberElement.classList.remove("positive-value");
-    } else {
-      receberElement.classList.add("positive-value");
-      receberElement.classList.remove("negative-value");
-    }
+    receberElement.classList.toggle("negative-value", totalReceivable < 0);
+    receberElement.classList.toggle("positive-value", totalReceivable >= 0);
   } else {
     console.error("Elemento para contas a receber não encontrado.");
   }
@@ -193,21 +177,14 @@ export const updateContasAReceber = () => {
 // Função para atualizar contas a pagar
 export const updateContasAPagar = () => {
   const totalPayable = calculatePayableAccounts();
-
-  console.log("Contas a Pagar Atualizadas:", totalPayable);
-
   const pagarElement = document.querySelector(
     ".cards-container .cards--right .card__value"
   );
+
   if (pagarElement) {
     pagarElement.textContent = totalPayable.toFixed(2);
-    if (totalPayable < 0) {
-      pagarElement.classList.add("negative-value");
-      pagarElement.classList.remove("positive-value");
-    } else {
-      pagarElement.classList.add("positive-value");
-      pagarElement.classList.remove("negative-value");
-    }
+    pagarElement.classList.toggle("negative-value", totalPayable < 0);
+    pagarElement.classList.toggle("positive-value", totalPayable >= 0);
   } else {
     console.error("Elemento para contas a pagar não encontrado.");
   }
@@ -219,6 +196,18 @@ export const updateAll = () => {
   updateFaturasDoMes();
   updateContasAReceber();
   updateContasAPagar();
+};
+
+export const updateTotalUI = () => {
+  const totalExpenses = calculateTotalExpenses();
+  const totalIncomes = calculateTotalIncomes();
+
+  document.querySelector(
+    ".box__income"
+  ).textContent = `R$ ${totalIncomes.toFixed(2)}`;
+  document.querySelector(
+    ".box__expense"
+  ).textContent = `R$ ${totalExpenses.toFixed(2)}`;
 };
 
 const setCurrentMonth = () => {
@@ -240,20 +229,17 @@ const setCurrentMonth = () => {
   const currentDate = new Date();
   const currentMonthName = monthNames[currentDate.getMonth()];
 
-  // Seleciona todos os elementos com a classe 'current-month'
   const monthElements = document.querySelectorAll(".current-month");
 
-  // Verifica se há elementos e os atualiza com o nome do mês
   monthElements.forEach((monthElement) => {
     monthElement.textContent = currentMonthName;
   });
-
-  console.log("Mês Atual Atualizado para:", currentMonthName);
 };
 
 // Chamar a função ao carregar a página
 document.addEventListener("DOMContentLoaded", updateAll);
 document.addEventListener("DOMContentLoaded", setCurrentMonth);
+document.addEventListener("DOMContentLoaded", updateTotalUI);
 
 // Função para retornar todas as transações
 export const getTransactions = () => transactions;
